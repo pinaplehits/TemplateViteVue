61010dee053adea4aa3932500f74cef486c6b268
Roberto Pina <roberto.pina@foxconn.com>
2024-06-19 10:46:09 -0600

    the table inside the esop now is component, to be used by another components

---
+++ src/components/DataManagementTable.vue
@@ -0,0 +1,173 @@
+<script setup>
+  import { ref, watch } from 'vue'
+
+  const loading = defineModel('loading', { type: Boolean, required: true })
+  const search = defineModel('search', { type: String, required: true })
+
+  const emit = defineEmits(['deleteItem'])
+
+  const props = defineProps({
+    items: { type: Array, required: true },
+    headers: { type: Array, required: true }
+  })
+
+  const sortBy = ref([])
+  const selected = ref([])
+
+  const toggleSort = (key) => {
+    let order = 'asc'
+    if (sortBy.value[0] && sortBy.value[0].key === key) {
+      if (sortBy.value[0].order === 'asc') {
+        order = 'desc'
+      } else {
+        sortBy.value = []
+        return
+      }
+    }
+
+    sortBy.value = [{ key, order }]
+  }
+
+  const sortIcon = (key) => {
+    if (sortBy.value[0]?.key === key) {
+      return sortBy.value[0]?.order === 'asc'
+        ? 'mdi-arrow-up'
+        : 'mdi-arrow-down'
+    }
+
+    return 'mdi-arrow-up'
+  }
+
+  watch(search, () => {
+    selected.value = []
+  })
+</script>
+
+<template>
+  <v-data-table
+    :items="props.items"
+    :headers="props.headers"
+    :loading="loading"
+    :search="search"
+    :sort-by="sortBy"
+    v-model="selected"
+    hover
+    show-select
+    fixed-header
+    items-per-page="-1"
+    hide-default-footer
+    density="compact"
+    return-object
+  >
+    <template #loading>
+      <v-skeleton-loader type="table-row-divider@10" />
+    </template>
+    <template #headers="{ columns, allSelected, selectAll, someSelected }">
+      <tr>
+        <template
+          v-for="column in columns"
+          :key="column.key"
+        >
+          <template v-if="column.value === 'data-table-select'">
+            <th>
+              <v-checkbox-btn
+                :indeterminate="someSelected && !allSelected"
+                :model-value="allSelected"
+                color="primary"
+                @update:model-value="selectAll(!allSelected)"
+                @click.stop
+              />
+            </th>
+          </template>
+          <template v-else-if="column.key === 'Actions'">
+            <th>
+              {{ column.title }}
+            </th>
+          </template>
+          <template v-else>
+            <th
+              class="header-no-wrap"
+              @click.stop="toggleSort(column.key)"
+            >
+              {{ column.title }}
+              <v-icon
+                small
+                class="sort-icon"
+                :class="{ 'is-sorting': sortBy[0]?.key === column.key }"
+              >
+                {{ sortIcon(column.key) }}
+              </v-icon>
+            </th>
+          </template>
+        </template>
+      </tr>
+    </template>
+    <template #item="{ item, columns }">
+      <tr @click="() => console.log(item.id)">
+        <template
+          v-for="column in columns"
+          :key="column.key"
+        >
+          <template v-if="column.key === 'data-table-select'">
+            <td style="width: 0px">
+              <v-checkbox-btn
+                v-model="selected"
+                :value="item"
+                color="primary"
+                @click.stop
+              />
+            </td>
+          </template>
+          <template v-else-if="column.key === 'Actions'">
+            <td style="text-align: center; width: 0px">
+              <v-icon
+                size="small"
+                style="color: maroon"
+                @click.stop="() => emit('deleteItem', item.id)"
+              >
+                mdi-delete
+              </v-icon>
+            </td>
+          </template>
+          <template v-else>
+            <td>
+              {{ item[column.key] }}
+            </td>
+          </template>
+        </template>
+      </tr>
+    </template>
+  </v-data-table>
+</template>
+
+<style scoped>
+  .v-data-table {
+    height: calc(98vh - 165px);
+  }
+
+  :deep(th) {
+    background-color: lavender !important;
+    font-weight: bold !important;
+  }
+
+  .header-no-wrap {
+    cursor: pointer;
+    white-space: nowrap;
+  }
+
+  .header-no-wrap:hover .sort-icon:not(.is-sorting) {
+    visibility: visible;
+    opacity: 0.5;
+  }
+
+  .sort-icon {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  .is-sorting {
+    visibility: visible;
+    opacity: 1;
+  }
+</style>
--- src/pages/EsopAdminPage.vue
+++ src/pages/EsopAdminPage.vue
@@ -3,11 +3,10 @@
   import apiClient from '@utils/axiosConfig.js'
   import { convertDateProperties } from '@utils/dateUtils.js'
   import ConfirmPassword from '@components/ConfirmPassword.vue'
+  import DataManagementTable from '@components/DataManagementTable.vue'
 
-  const sortBy = ref([])
   const search = ref('')
   const searchFocused = ref(false)
-  const selected = ref([])
   const loading = ref(false)
   const loadingCreateForm = ref(false)
   const form = ref(null)
@@ -139,35 +138,7 @@
     }
   }
 
-  const toggleSort = (key) => {
-    let order = 'asc'
-    if (sortBy.value[0] && sortBy.value[0].key === key) {
-      if (sortBy.value[0].order === 'asc') {
-        order = 'desc'
-      } else {
-        sortBy.value = []
-        return
-      }
-    }
-
-    sortBy.value = [{ key, order }]
-  }
-
-  const sortIcon = (key) => {
-    if (sortBy.value[0]?.key === key) {
-      return sortBy.value[0]?.order === 'asc'
-        ? 'mdi-arrow-up'
-        : 'mdi-arrow-down'
-    }
-
-    return 'mdi-arrow-up'
-  }
-
   onMounted(loadData)
-
-  watch(search, () => {
-    selected.value = []
-  })
 
   watch(showCreateProject, (newValue) => {
     if (!newValue) form.value.reset()
@@ -212,7 +183,6 @@
           </v-col>
         </v-row>
       </v-container>
-
       <v-dialog
         :persistent="loadingCreateForm"
         v-model="showCreateProject"
@@ -295,132 +265,13 @@
         </v-card>
       </v-dialog>
       <v-divider />
-      <v-data-table
+      <DataManagementTable
         :items="itemsSop"
         :headers="headersSop"
-        :loading="loading"
-        :search="search"
-        :sort-by="sortBy"
-        hover
-        show-select
-        v-model="selected"
-        fixed-header
-        items-per-page="-1"
-        hide-default-footer
-        density="compact"
-        return-object
-      >
-        <template #loading>
-          <v-skeleton-loader type="table-row-divider@10" />
-        </template>
-        <template #headers="{ columns, allSelected, selectAll, someSelected }">
-          <tr>
-            <template
-              v-for="column in columns"
-              :key="column.key"
-            >
-              <template v-if="column.value === 'data-table-select'">
-                <th>
-                  <v-checkbox-btn
-                    :indeterminate="someSelected && !allSelected"
-                    :model-value="allSelected"
-                    color="primary"
-                    @update:model-value="selectAll(!allSelected)"
-                    @click.stop
-                  />
-                </th>
-              </template>
-              <template v-else-if="column.key === 'Actions'">
-                <th>
-                  {{ column.title }}
-                </th>
-              </template>
-              <template v-else>
-                <th
-                  class="header-no-wrap"
-                  @click.stop="toggleSort(column.key)"
-                >
-                  {{ column.title }}
-                  <v-icon
-                    small
-                    class="sort-icon"
-                    :class="{ 'is-sorting': sortBy[0]?.key === column.key }"
-                  >
-                    {{ sortIcon(column.key) }}
-                  </v-icon>
-                </th>
-              </template>
-            </template>
-          </tr>
-        </template>
-        <template #item="{ item, columns }">
-          <tr @click="() => console.log(item.id)">
-            <template
-              v-for="column in columns"
-              :key="column.key"
-            >
-              <template v-if="column.key === 'data-table-select'">
-                <td style="width: 0px">
-                  <v-checkbox-btn
-                    v-model="selected"
-                    :value="item"
-                    color="primary"
-                    @click.stop
-                  />
-                </td>
-              </template>
-              <template v-else-if="column.key === 'Actions'">
-                <td style="text-align: center; width: 0px">
-                  <v-icon
-                    size="small"
-                    style="color: maroon"
-                    @click.stop="deleteItem(item.id)"
-                  >
-                    mdi-delete
-                  </v-icon>
-                </td>
-              </template>
-              <template v-else>
-                <td>
-                  {{ item[column.key] }}
-                </td>
-              </template>
-            </template>
-          </tr>
-        </template>
-      </v-data-table>
+        v-model:loading="loading"
+        v-model:search="search"
+        @delete-item="deleteItem"
+      />
     </v-card>
   </v-container>
 </template>
-
-<style scoped>
-  .v-data-table {
-    height: calc(100vh - 165px);
-  }
-
-  :deep(th) {
-    background-color: lavender !important;
-    font-weight: bold !important;
-  }
-
-  .header-no-wrap {
-    cursor: pointer;
-    white-space: nowrap;
-  }
-
-  .header-no-wrap:hover .sort-icon:not(.is-sorting) {
-    visibility: visible;
-    opacity: 0.5;
-  }
-
-  .sort-icon {
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity 0.3s;
-  }
-
-  .is-sorting {
-    visibility: visible;
-    opacity: 1;
-  }
-</style>
