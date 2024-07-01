fee944e806e9c5c7136bb1ddb963415022da1cc1
Roberto Pina <roberto.pina@foxconn.com>
2024-06-30 23:48:16 -0600

    refactor: Replace CreateModelForm with GenericForm in ModelsAdminPage.vue

--- src/pages/ModelsAdminPage.vue
+++ src/pages/ModelsAdminPage.vue
@@ -3,17 +3,24 @@
   import { populateAdminTable } from '@utils/tableUtils'
   import ConfirmPassword from '@components/ConfirmPassword.vue'
   import DataManagementTable from '@components/DataManagementTable.vue'
-  import CreateModelForm from '@components/CreateModelForm.vue'
+  import GenericForm from '@components/GenericForm.vue'
 
   const items = ref([])
   const headers = ref([])
   const currentItem = ref({ assemblyDellModel: {} })
   const loading = ref(false)
-  const showCreate = ref(false)
+  const showForm = ref(false)
   const showConfirmPassword = ref(false)
 
   const endpointDelete = 'AssemblyDell/DeleteModel'
   const endpointGet = 'AssemblyDell/GetModels'
+
+  const form = ref({
+    title: 'Create Model',
+    buttonText: 'Create model',
+    endpoint: 'AssemblyDell/CreateModel',
+    data: {}
+  })
 
   const loadData = async () => {
     loading.value = true
@@ -44,16 +51,35 @@
     :data="currentItem"
     @success="loadData"
   />
-  <CreateModelForm
-    v-model:showForm="showCreate"
-    @success="loadData"
-  />
+  <GenericForm
+    v-model:showForm="showForm"
+    v-bind="form"
+    @submit-success="loadData"
+  >
+    <v-text-field
+      autofocus
+      v-model="form.data.ModelName"
+      class="mx-4 mb-2"
+      variant="solo"
+      label="Model name"
+      :rules="[(value) => !!value || 'Model name is required']"
+      :disabled="loading"
+    />
+    <v-text-field
+      v-model="form.data.Platform"
+      class="mx-4"
+      variant="solo"
+      label="Platform name"
+      :rules="[(value) => !!value || 'Platform name is required']"
+      :disabled="loading"
+    />
+  </GenericForm>
   <DataManagementTable
     :items="items"
     :headers="headers"
     text-add-button="Create model"
     v-model:loading="loading"
-    v-model:showForm="showCreate"
+    v-model:showForm="showForm"
     @delete-item="deleteItem"
   />
 </template>
