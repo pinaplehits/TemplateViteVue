e1db7740221aac7c6c76f83fb1570b27697fa364
Roberto Pina <roberto.pina@foxconn.com>
2024-07-01 00:03:00 -0600

    refactor: Replace CreateLineForm with GenericForm in LinesAdminPage.vue

--- src/pages/LinesAdminPage.vue
+++ src/pages/LinesAdminPage.vue
@@ -3,17 +3,24 @@
   import { populateAdminTable } from '@utils/tableUtils'
   import ConfirmPassword from '@components/ConfirmPassword.vue'
   import DataManagementTable from '@components/DataManagementTable.vue'
-  import CreateLineForm from '@components/CreateLineForm.vue'
+  import GenericForm from '@components/GenericForm.vue'
 
   const items = ref([])
   const headers = ref([])
   const currentItem = ref({ assemblyDellLine: {} })
   const loading = ref(false)
-  const showCreate = ref(false)
+  const showForm = ref(false)
   const showConfirmPassword = ref(false)
 
   const endpointDelete = 'AssemblyDell/DeleteLine'
   const endpointGet = 'AssemblyDell/GetLines'
+
+  const form = ref({
+    title: 'Create Line',
+    buttonText: 'Create line',
+    endpoint: 'AssemblyDell/CreateLine',
+    data: {}
+  })
 
   const loadData = async () => {
     loading.value = true
@@ -44,16 +51,35 @@
     :data="currentItem"
     @success="loadData"
   />
-  <CreateLineForm
-    v-model:showForm="showCreate"
-    @success="loadData"
-  />
+  <GenericForm
+    v-model:showForm="showForm"
+    v-bind="form"
+    @submit-success="loadData"
+  >
+    <v-text-field
+      autofocus
+      v-model="form.data.LineName"
+      class="mx-4 mb-2"
+      variant="solo"
+      label="Line name"
+      :rules="[(value) => !!value || 'Line name is required']"
+      :disabled="loading"
+    />
+    <v-text-field
+      v-model="form.data.Type"
+      class="mx-4"
+      variant="solo"
+      label="Type"
+      :rules="[(value) => !!value || 'Type is required']"
+      :disabled="loading"
+    />
+  </GenericForm>
   <DataManagementTable
     :items="items"
     :headers="headers"
     text-add-button="Create line"
     v-model:loading="loading"
-    v-model:showForm="showCreate"
+    v-model:showForm="showForm"
     @delete-item="deleteItem"
   />
 </template>
